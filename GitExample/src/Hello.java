7d59ecfbba85530664ceebb17026f7e48062b85f
Rahul <rg173602@gmail.com>
2021-03-23 16:36:58 +0530

    Addition of two integers

--- GitExample/src/Hello.java
+++ GitExample/src/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
 
 	public void method1() {
 		System.out.println("World");
+		int c =10+20;
+		System.out.println(c);
 	}
 	public static void main(String[] args) {
 		System.out.println("Hello");
