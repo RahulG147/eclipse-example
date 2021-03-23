a6702abe7c1976a823b4a3d1cbcb8611150aa519
Rahul <rg173602@gmail.com>
2021-03-23 17:48:01 +0530

    Merge conflict

--- GitExample/src/Hello.java
+++ GitExample/src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		System.out.println("World");
 		int c =10+20;
 		
-		System.out.println("method1 prints World and "+c);
+		System.out.println("method1 prints World and variable"+c);
 	}
 	public static void main(String[] args) {
 		System.out.println("Hello");
