9461b71ee01569287e7619bafa7b25f3b189e3f5
Rahul <rg173602@gmail.com>
2021-03-23 17:11:30 +0530

    Some few lines of codes

--- GitExample/src/Hello.java
+++ GitExample/src/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 		System.out.println("World");
 		int c =10+20;
 		System.out.println(c);
+		System.out.println("method1 prints World and "+c);
 	}
 	public static void main(String[] args) {
 		System.out.println("Hello");
