e23b685d1f27549c8be38d4b7698ec796a03e079
Rahul <rg173602@gmail.com>
2021-03-23 17:19:19 +0530

    Deleted some line of code

--- GitExample/src/Hello.java
+++ GitExample/src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public void method1() {
 		System.out.println("World");
 		int c =10+20;
-		System.out.println(c);
+		
 		System.out.println("method1 prints World and "+c);
 	}
 	public static void main(String[] args) {
